cc589ff424dbc0cdfc01608bb2ecd0c6dc269a55
Sam <sam@Sams-MacBook-Pro.local>
2025-10-09 16:48:48 +0100

    zod

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -7,11 +7,21 @@ import {
   deleteProductById,
 } from "./controler";
 
+import { validateData } from "../../middleweres/validationMiddlewere";
+import z from "zod";
+
+export const productSchema = z.object({
+  name: z.string().min(1, "name cannot be empty"),
+  price: z.number().min(0, "price must be a positive number"),
+  description: z.string().min(1, "description cannot be empty"),
+  inStock: z.number().min(0, "inStock must be a positive number"),
+});
+
 const router = Router();
 
 router.get("/", getAllProducts);
 
-router.post("/", createProduct);
+router.post("/", validateData(productSchema), createProduct);
 
 router.get("/:id", getProductById);
 
